27ccf4951db0732df862d92d65bc2f98bdedb7ec
Karl Roe <103881268+K-Roe@users.noreply.github.com>
2022-05-30 21:41:22 +0100

    added if statement incase odd number was given

--- Heads_tails.py
+++ Heads_tails.py
@@ -5,21 +5,34 @@ import matplotlib.pyplot as plt
 # Coin list
 heads_Counter = 0
 tails_Counter = 0
-test_run_times = 100
+test_run_times = int(input("how many times would you like to flip the coin"))
 test = 1
 coin =("Heads", "Tails")
-while test <= test_run_times:
-    test += 1
-    if random.choice(coin) == "Heads":
-        heads_Counter +=1
-    else:
-        tails_Counter +=1
 
+if (test_run_times % 2) == 0:
+    while test <= test_run_times:
+        test += 1
+        if random.choice(coin) == "Heads":
+            heads_Counter +=1
+        else:
+            tails_Counter +=1
+elif (test_run_times % 2) == 1:
+    test_run_times += 1
+    print("Must choice a even number I will add one on to your number to make it even thank you")
+    while test <= test_run_times:
+        test += 1
+        if random.choice(coin) == "Heads":
+            heads_Counter +=1
+        else:
+            tails_Counter +=1
+
+total = heads_Counter + tails_Counter
 
 #create data
 
 data = [["Heads", heads_Counter],
-        ["Tails", tails_Counter]]
+        ["Tails", tails_Counter],
+        ["Total times flipped", total]]
 #header names
 col_names = ["Coin", "Times flipped"]
 #disply table
