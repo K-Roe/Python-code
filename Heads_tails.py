73a3a01125203552d098a27c08faa738f80146a8
Karl Roe <103881268+K-Roe@users.noreply.github.com>
2022-05-30 20:38:33 +0100

    Add files via upload

--- Heads_tails.py
+++ Heads_tails.py
@@ -1,13 +1,14 @@
 import random
-
+from tabulate import tabulate
+import matplotlib.pyplot as plt
 
 # Coin list
 heads_Counter = 0
 tails_Counter = 0
 test_run_times = 100
-test = 0
+test = 1
 coin =("Heads", "Tails")
-while test <=99:
+while test <= test_run_times:
     test += 1
     if random.choice(coin) == "Heads":
         heads_Counter +=1
@@ -15,6 +16,19 @@ while test <=99:
         tails_Counter +=1
 
 
+#create data
 
-print(f"The outcome from the Coin flipped 100 times is Heads: {heads_Counter} Times. and Tails: {tails_Counter} Times.")
+data = [["Heads", heads_Counter],
+        ["Tails", tails_Counter]]
+#header names
+col_names = ["Coin", "Times flipped"]
+#disply table
+print(tabulate(data, headers=col_names))
 
+#Create pie chart to show outcome
+labels = ["Heads", "Tails"]
+Flipped = [heads_Counter, tails_Counter]
+plt.pie(Flipped, labels=labels,
+        autopct='%1.1f%%', shadow=True, startangle=140)
+plt.axis('equal')
+plt.show()
